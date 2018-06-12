98d405bda6bfd8d3b2bdfaf611b6fc59c7bdfb53
bhuvnesh25 <bhuvneshkumar2798@gmail.com>
2018-06-12 22:20:35 +0530

    assignment-1 and assignment-2 are completed

---
+++ assignment-2.py
@@ -0,0 +1,45 @@
+# Q1-Print anything you want on screen.
+
+print("Hello my name is bhuvnesh kumar")
+print("i started learning python in this summer training")
+print("i think so python is best programming language for statup purpose")
+print("as, it is easy to learn")
+print("it is interpreted language")
+
+# Q2-Join twop string using '+' E.g.-"Acad"+"View".
+
+x="Bhuvnesh"
+y="Kumar"
+print(x,"+",y)
+z=x+y
+print(z)
+
+# Q3-Take the input of 3 variable x,y and z.Print their values on screen.
+
+x=str(input("enter name:"))
+y=int(input("enter roll-no:"))
+z=int(input("enter phone no.:"))
+print(x,y,z)
+
+x=int(input("enter 1st no:"))
+y=int(input("enter 2nd no:"))
+z=int(input("enter 3rd no.:"))
+print(x,y,z)
+
+# Q4-Print "Let's get started on screen".
+
+print("Let\'s get started")
+print("hey friend\'s")
+
+# Q5-Print the following values using placeholders.s="Acadview" course="Python" Fees=5000.
+
+x="Acadview course"
+y="Python"
+z="Fees-5000"
+print("%s %s %s"%(x,y,z)) 
+
+# Q6-Let's do interesting exercise.name="Tony stark" salary=1000000 print('%s''%d')%(____,____).
+
+name="Tony stark"
+salary=1000000
+print("%s , %d"%(name,salary))
